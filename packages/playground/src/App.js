2cdb333df8d83dd53ab32742814cacb32b576ce0
William Wong <compulim@hotmail.com>
2018-06-16 01:21:27 -0700

    Fix publish

--- packages/playground/src/App.js
+++ packages/playground/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import MyComponent from 'my-component';
+import MyComponent from 'component';
 
 class App extends Component {
   render() {
